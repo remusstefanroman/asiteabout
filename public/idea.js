4799b5e8912d4bb8d7cef077196040a60a6f5cac
remusstefanroman <roman.remus.stefan@gmail.com>
2020-06-08 13:26:05 +0300

    yes

--- public/idea.js
+++ public/idea.js
@@ -1,100 +1,178 @@
-
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-      $(".zoom img").css({
+$(document).ready(function(){
+  var scrollable=950;
+  let outroscroll=200;
+    let contentheight = $(window).height()+scrollable+outroscroll;
+    $("#scrollingSpace").css({
+        height:contentheight
+    });
+    console.log(contentheight);
+    
+    let panels = [
+        {
+          name: "panel1",
+          zi: 3600,
+          xi: -700,
+          yi: -1300,
+          SN: 0,
+         
+        },
+        {
+            name: "panel2",
+            zi: 2000,
+            xi: -200,
+            yi: -400,
+            SN: 500,
           
-          transform: 'translate3d(0, -'+(35+scroll/300)+'%, 0) scale('+(300-(scroll/8.5))/100+')',
-          //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-         // "-webkit-filter": "blur(" + (scroll/200) + "px)",
-         // filter: "blur(" + (scroll/200) + "px)"
-      });
-      
-      function fade(location){
-        if((scroll<=location-300)||(scroll>=location+300)){
-            return 0;
-        }else{
-            if(location<=scroll&&scroll<=location+100){
-                return 1;
-            }else{
-                if(scroll<location){
-                    return ((300-(location-scroll))/300)
-                }else{
-                    return((300-(scroll-location))/200  )
-                }
-            }
-        }
-      };
-      
-      
-      function dist(x){
-         return(Math.abs(x-scroll));
-      };
-      
-      
-      function poceala(location){
-        if((scroll<=location-300)||(scroll>=location+300)){
-            return 0;
-        }else{
-            if(location<=scroll&&scroll<=location+100){
-                return 0;
-            }else{
-                if(scroll<location){
-                    return (dist(location)/100)
-                }else{
-                    return((dist(location)-100)/100)
-                }
-            }
-        }
-      };
-
-        $(".panel1").css({
+          },
+          {
+            name: "panel3",
+            zi: $( window ).width(),
+            xi: -200,
+            yi: 0,
+            SN: scrollable,
             
-            transform: 'translate3d('+(35-(scroll/30))+'%, '+(200-(scroll/15))+'%, 0) scale('+(300-(scroll/8.5))/100+')',
-            //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-            "-webkit-filter": "blur(" + (poceala(0)) + "px)",
-            filter: "blur(" + (poceala(0)) + "px)",
-            opacity: fade(0)
-        });
-
-        $(".panel2").css({
+          },
+          {
+            name: "final",
+            zi: $( window ).width(),
+            xi: -200,
+            yi:0,
+            SN: scrollable+outroscroll,
             
-            transform: 'translate3d('+(35-(scroll/140))+'%, '+(180-(scroll/15))+'%, 0) scale('+(300-(scroll/8.5))/100+')',
-            //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-            "-webkit-filter": "blur(" + (poceala(500)) + "px)",
-            filter: "blur(" + (poceala(500)) + "px)",
-            opacity: fade(500)
-        });
+          }
+      ];
 
-        $(".panel3").css({
-            
-            transform: 'translate3d('+(35-(scroll/140))+'%, '+(160-(scroll/15))+'%, 0) scale('+(300-(scroll/8.5))/100+')',
-            //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-            "-webkit-filter": "blur(" + (poceala(1000)) + "px)",
-            filter: "blur(" + (poceala(1000)) + "px)",
-            opacity: fade(1000)
-        });
+let bgz = [
+      {
+        name:'bg1',
+        Si:0,
+        Sf:500
         
-        $(".panel4").css({
-            
-            transform: 'translate3d('+(35-(scroll/140))+'%, '+(150-(scroll/15))+'%, 0) scale('+(300-(scroll/8.5))/100+')',
-            //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
-            "-webkit-filter": "blur(" + (poceala(1600)) + "px)",
-            filter: "blur(" + (poceala(1600)) + "px)",
-            opacity: fade(1600)
-        });
+      },
+      {
+        name:'bg2',
+        Si:500,
+        Sf:850
+      },
+      {
+        name:'bg3',
+        Si:850,
+        Sf:10000
+      }
+
+    ]
+      
+    let cassettes = [
+      {
+        name:'cassette1',
+        Si:0,
+        Sf:100,
+        in:false
+      },
+      {
+        name:'cassette2',
+        Si:500,
+        Sf:650,
+        in:false
+      },
+      {
+        name:'cassette3',
+        Si:850,
+        Sf:1000,
+        in:false
+      }
+
+    ]
+
+      $("#board").css('top',panels[0].xi+'px');
+
+      $("#board").css('left',panels[0].yi+'px');
+
+      document.getElementById('board').style.width = panels[0].zi+"px";
+
+      document.getElementById('board').style.height = (panels[0].zi/3)*2+"px";
+
+      var init=$(window).scrollTop();
+
+      setTimeout(() => {for(k=0;k<cassettes.length;k+=1){
+
+        if((cassettes[k].Si<=init)&&(cassettes[k].Sf>init)){
+          if(!cassettes[k].in==true){
+          console.log('fadein cassette '+cassettes[k].name)
+          document.getElementById(cassettes[k].name).classList.remove("fade-out-top");
+          document.getElementById(cassettes[k].name).classList.add("fade-in-bottom");
+          cassettes[k].in=true;}
+
+        }else{
+          if(cassettes[k].in==true){
+          cassettes[k].in=false;
+          document.getElementById(cassettes[k].name).classList.remove("fade-in-bottom");
+          document.getElementById(cassettes[k].name).classList.add("fade-out-top");
+          console.log('fadeout cassette '+cassettes[k].name)
+      }}
+         
+
+      } 
+      }, 2000);
+      
+      $(window).scroll(function() {
         
+        var scroll = $(window).scrollTop();
+        console.log(scroll);
+        if(scroll<=scrollable+outroscroll){
+         for(i=1;i<panels.length;i+=1){
+            if((panels[i-1].SN<=scroll)&&(scroll<panels[i].SN)){
+              
+             $("#board").css('top',panels[i-1].xi+(scroll-panels[i-1].SN)*((panels[i].xi-panels[i-1].xi)/(panels[i].SN-panels[i-1].SN))+'px');
+             $("#board").css('left',panels[i-1].yi+(scroll-panels[i-1].SN)*((panels[i].yi-panels[i-1].yi)/(panels[i].SN-panels[i-1].SN))+'px');
+             document.getElementById('board').style.width = panels[i-1].zi+(scroll-panels[i-1].SN)*((panels[i].zi-panels[i-1].zi)/(panels[i].SN-panels[i-1].SN))+"px";
+             document.getElementById('board').style.height = (panels[i-1].zi+(scroll-panels[i-1].SN)*((panels[i].zi-panels[i-1].zi)/(panels[i].SN-panels[i-1].SN)))*2/3+'px';
+             document.getElementById('board').style.fontSize = (panels[i-1].zi+(scroll-panels[i-1].SN)*((panels[i].zi-panels[i-1].zi)/(panels[i].SN-panels[i-1].SN)))/225+'px';
+           }
+          }
+          
+        for(j=0;j<bgz.length;j+=1){
+
+          if(bgz[j].Si-20<=scroll&&bgz[j].Sf>scroll){
+            if(scroll>=bgz[j].Sf-20){$('#'+bgz[j].name).css({opacity:(bgz[j].Sf-scroll)/20})}else{
+            $('#'+bgz[j].name).css({opacity:1})}}
+            else{$('#'+bgz[j].name).css({opacity:0})};
+
+        }
+
+        for(k=0;k<cassettes.length;k+=1){
+
+          if((cassettes[k].Si<=scroll)&&(cassettes[k].Sf>scroll)){
+            if(!cassettes[k].in==true){
+            console.log('fadein cassette '+cassettes[k].name)
+            document.getElementById(cassettes[k].name).classList.remove("fade-out-top");
+            document.getElementById(cassettes[k].name).classList.add("fade-in-bottom");
+            cassettes[k].in=true;}
+
+          }else{
+            if(cassettes[k].in==true){
+            cassettes[k].in=false;
+            document.getElementById(cassettes[k].name).classList.remove("fade-in-bottom");
+            document.getElementById(cassettes[k].name).classList.add("fade-out-top");
+            console.log('fadeout cassette '+cassettes[k].name)
+        }}
+           
+
+              }
+            }else{
+              if(scroll>scrollable+outroscroll){
+                let x=$("#board").css('top');
+                $('#board').css('top',panels[panels.length-1].xi+scrollable+outroscroll-scroll+'px')
+                console.log($('#board').css('top'));
+        
+                
+              
+              }
+
+            }
+        
+        })
+      
+
 
   });
-
-  $(document).ready(function(){
-    let contentheight = screen.height+1500;
-    $("#content").css({
-        height:contentheight
-    })
-
-    $("html").css({
-        "font-size": (screen.height/80)+'px',
-    })
-  }
-  )
- 
--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const app = express();
 
-var mustacheExpress = require('mustache-express');
-app.engine('html',mustacheExpress());
-app.set('view engine','html');
-app.set('views', __dirname + '/public');
-app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT || 3000;
 
-
-
-app.get('/idea',(req,res)=>{
-res.render('idea.mustache');
-});
+app.use(express.static('public'));
 
 app.listen(port,()=>{
     console.log('Hey Boss')
